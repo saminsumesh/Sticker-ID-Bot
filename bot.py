2803b732fbd8a12f0a8f9ff6d5b2468637d38a5e
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-26 01:10:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -91,7 +91,7 @@ async def about(bot, message):
 @Bot.on_message(filters.sticker & filters.private)
 async def sticker(bot, message):
     await message.reply_sticker(
-    sticker=f"Here is your sticker id :" {message.sticker.file_id}
+    sticker="Here is your sticker id :{message.sticker.file_id}"
     )
     
 print("Bot is now running, If any issues contact us through @XD_Botzsupport")
